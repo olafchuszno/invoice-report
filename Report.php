8cc2f33aaff40cc3fd418085bf181436488a806e
Olaf <olaf.chuszno@gmail.com>
2024-09-26 10:30:57 +0200

    Implement getting overpayments in Report func

--- Report.php
+++ Report.php
@@ -1,5 +1,20 @@
 <?php 
 
 class Report {
+  private Database $db;
+  private $valid_sort_orders = ['ASC', 'DESC'];
 
+  public function __construct(Database $db)
+  {
+    $this->db = $db;
+  }
+
+  function getOverpayments($sortBy, $order): array {
+    return $this->db->query("
+      SELECT c.company_name, o.overpayment_amount FROM overpayments o
+      JOIN customers c
+      ON o.customer_id = c.id
+      ORDER BY $sortBy $order
+    ");
+  }
 }
