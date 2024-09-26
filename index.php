3c63508a3546c64703f42a9f35a359fd2192f864
Olaf <olaf.chuszno@gmail.com>
2024-09-26 14:48:38 +0200

    Create CSS file

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css"></link>
   <title>Raporty</title>
 </head>
 
 <body>
-  <h1>Raporty</h1>
+  <h1 class="title">Raporty</h1>
 
   <h2>Nadpłaty</h2>
 
